a37c1413c0af220e13ef8a69cd7ad1dd9fcae8e9
Hung Nguyen <nguyenht33@gmail.com>
2020-11-25 12:19:59 -0500

    Simplified components and added axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import User from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  render() {
-    const name = 'John Doe';
+  state = {
+    users: [],
+    loading: false
+  }
 
+  async componentDidMount() {
+    const res = await axios.get('https://api.github.com/users');
+    console.log(res.data)
+  }
+
+  render() {
     return (
       <div className='App'>
         <Navbar />
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,8 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const UserItem = props => {    
-    const { login, avatar_url, html_url } = props.user;
-
+const UserItem = ({user: {login, avatar_url, html_url }}) => {    
     return (
         <div className="card text-center">
             <img 
@@ -16,6 +15,10 @@ const UserItem = props => {
             </div>
         </div>
     )
+}
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired,
 }
 
 export default UserItem;
