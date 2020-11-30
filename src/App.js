d97ba7d69bbdda3afbf424b3fc6071dd9bd18b69
Hung Nguyen <nguyenht33@gmail.com>
2020-11-30 10:47:35 -0500

    added search alert

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import User from './components/users/Users';
-import Search from './components/users/Search'
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null,
   }
 
   searchUsers = async text => {
@@ -22,15 +24,32 @@ class App extends Component {
 
   clearUsers = () => this.setState({ users: [], loading: false });
 
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  } 
+
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, alert } = this.state;
 
     return (
       <div className='App'>
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={ users.length > 0 ? true: false }/>
-          <User loading={loading} users={users} />
+          <Alert 
+            alert={alert}
+          />
+          <Search 
+            searchUsers={this.searchUsers} 
+            clearUsers={this.clearUsers} 
+            showClear={ users.length > 0 ? true: false }
+            setAlert={this.setAlert}
+          />
+          <User 
+            loading={loading} 
+            users={users} 
+          />
         </div>
       </div>
     );
