b5e3a69a37f36763c8c0624c77b23b42632d4d8d
Hung Nguyen <nguyenht33@gmail.com>
2020-11-30 14:20:28 -0500

    added repos component

--- src/App.js
+++ src/App.js
@@ -13,9 +13,10 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null,
-  }
+  };
 
   searchUsers = async text => {
     this.setState({ loading: true })
@@ -33,6 +34,14 @@ class App extends Component {
     this.setState({ user: res.data, loading: false })
   }
 
+  getUserRepos = async username => {
+    this.setState({ loading: true })
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secrete=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ repos: res.data, loading: false })
+  }
+
   clearUsers = () => this.setState({ users: [], loading: false });
 
   setAlert = (msg, type) => {
@@ -41,7 +50,7 @@ class App extends Component {
   } 
 
   render() {
-    const { users, user, loading, alert } = this.state;
+    const { users, user, repos, loading, alert } = this.state;
 
     return (
       <Router>
@@ -66,7 +75,10 @@ class App extends Component {
                 <User 
                   {...props} 
                   getUser={this.getUser} 
+                  getUserRepos={this.getUserRepos}
                   user={user}
+                  repos={repos}
+                  loading={loading}
                 />
               )}/>
             </Switch>
