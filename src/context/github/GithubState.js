af1b719b44d28eac40c61fc077111caaed9cc144
Hung Nguyen <nguyenht33@gmail.com>
2020-12-01 15:39:21 -0500

    converted state components to context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -36,6 +36,33 @@ const GithubState = props => {
         });
     };
 
+    const getUser = async username => {
+        setLoading();
+    
+        const res = await axios.get(
+          `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secrete=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
+    
+        dispatch({
+            type: GET_USER,
+            payload: res.data,
+        }); 
+      };
+
+    const getUserRepos = async username => {
+        setLoading();
+        const res = await axios.get(
+            `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secrete=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
+
+        dispatch({
+            type: GET_REPOS,
+            payload: res.data,
+        });  
+    }; 
+
+    const clearUsers = () => dispatch({ type: CLEAR_USERS });
+
     const setLoading = () => dispatch({ type: SET_LOADING });    
 
     return <GithubContext.Provider
@@ -44,7 +71,10 @@ const GithubState = props => {
             user: state.user,
             repos: state.repos,
             loading: state.loading,
-            searchUsers
+            searchUsers,
+            getUser,
+            getUserRepos,
+            clearUsers
         }}
     >
         {props.children}
